e6bfdd27a0bf72fb495dcac146d4dfa4e35a0b1e
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2023-11-16 21:59:59 -0700

    server.js added

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // This should match your frontend URL
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+const passcodes = [];
+const Rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("message", (message) => {
+    console.log("Message received:", message.message, "from", message.sender);
+    socket.to(Rooms[socket.id]).emit("message", message);
+  });
+
+  socket.on("passcode", (passcode) => {
+    Rooms[socket.id] = passcode;
+    if (passcodes.includes(passcode)) {
+      socket.join(passcode);
+      console.log("Joined room", passcode);
+      io.to(passcode).emit("connection", true);
+    } else {
+      passcodes.push(passcode);
+      socket.join(passcode);
+      console.log("waiting in room", passcode);
+    }
+  });
+});
+
+server.listen(80, () => {
+  console.log("Listening on *:8080");
+});
